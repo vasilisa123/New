e8a202fc7f09822f84d7d2ea4d269aeb06aa99d2
Вадим <den.midav@gmail.com>
2024-05-02 22:25:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+let buttonBack = document.querySelector('.footer__block-back');
+let buttonNext = document.querySelector('.footer__block-next');
+let main__block1 = document.querySelector('.main__block1');
+let main__block2 = document.querySelector('.main__block2');
+let main__block3 = document.querySelector('.main__block3');
+
+buttonBack.addEventListener('click', function () {
+    buttonBack
+});
+
+// buttonNext.addEventListener('click', function () {
+//     buttonBack.classList.toggle('active');
+//     main__block1.classList.toggle('active');
+//     main__block2.classList.toggle('active');
+//     buttonNext.classList.toggle('active');
+    
+    
+// });
+// buttonBack.addEventListener('click', function () {
+//     buttonBack.classList.toggle('active');
+//     main__block1.classList.toggle('active');
+//     main__block2.classList.toggle('active');
+//     buttonNext.classList.toggle('active');
+// });
+
+// buttonNext.addEventListener('click', function () {
+//     buttonBack.classList.toggle('active');
+//     main__block1.classList.toggle('active');
+//     main__block3.classList.toggle('active');
+// });
+// buttonBack.addEventListener('click', function () {
+//     buttonBack.classList.toggle('active');
+//     main__block1.classList.toggle('active');
+//     main__block3.classList.toggle('active');
+// });
+
